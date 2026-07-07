2f14536bec98d60ed3723ae9c5f45eb79e201127
Naveen Dubey <n.k.dubey1997@gmail.com>
2026-07-08 00:01:32 +0530

    Delete app directory

--- app/src/main/assets/www/debug.js
+++
@@ -1,6 +0,0 @@
-window.onerror = function(message, source, lineno, colno, error) {
-  alert("Error: " + message + " at " + source + ":" + lineno + ":" + colno);
-};
-window.onunhandledrejection = function(event) {
-  alert("Unhandled Rejection: " + event.reason);
-};
